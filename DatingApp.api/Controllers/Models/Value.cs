d32d194533e2ce185d2966978af4f34f21484a9d
Mohammad Mohajerani <mohammadmohajerani@MacBook-Air.local>
2020-07-12 20:58:27 +0300

    create firdt Model and including Microsoft.EntityFrameworkCore & Microsoft.EntityFrameworkCore.Sqlite

---
+++ DatingApp.api/Controllers/Models/Value.cs
@@ -0,0 +1,8 @@
+namespace DatingApp.api.Controllers.Models
+{
+    public class Value
+    {
+        public int Id { get; set; }
+        public int Name { get; set; }
+    }
+}
---
+++ DatingApp.api/Data/DataContext.cs
@@ -0,0 +1,12 @@
+using DatingApp.api.Controllers.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.api.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext>options) : base(options){}
+        public DbSet<Value> Values { get; set; }
+
+    }
+}
