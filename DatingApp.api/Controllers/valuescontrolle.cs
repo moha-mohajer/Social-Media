f3baaffd656bae345fe019cb91f56981fe39c2ff
Mohammad Mohajerani <mohammadmohajerani@MacBook-Air.local>
2020-07-13 22:17:26 +0300

    Cratting database and retriving data from batabase and fix issue of reading Name 0

--- DatingApp.api/Controllers/valuescontrolle.cs
+++ DatingApp.api/Controllers/valuescontrolle.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DatingApp.api.Data;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DatingApp.API.Controllers
@@ -10,24 +11,35 @@ namespace DatingApp.API.Controllers
     [ApiController]
     public class ValuesController : ControllerBase
     {
+        private readonly DataContext _context;
+
+        public ValuesController(DataContext context)
+        {
+            _context = context;
+        }
         // GET api/values
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        public IActionResult GetValues()
         {
-            return new string[] { "value1", "value2" };
+            var values = _context.Values.ToList();
+
+            return Ok(values);
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
+        public IActionResult GetValue(int id)
         {
-            return "value";
+            var value =_context.Values.FirstOrDefault(x => x.Id == id);
+
+            return Ok(value);
         }
 
         // POST api/values
         [HttpPost]
         public void Post([FromBody] string value)
         {
+            
         }
 
         // PUT api/values/5
